481ccbae0d71745f778d16750fdeb75fd44d6720
robinaerts <nibor.aerts@gmail.com>
2024-02-08 20:10:57 +0100

    schema + motor

--- server/server.ino
+++ server/server.ino
@@ -1,21 +1,59 @@
-int VOCHTIGHEIDPIN = 0; // Bodemvochtigheidssensor (analoge input A0)
-int vochtigheid = 0; 
+int VOCHTIGHEIDSPIN = 34;  // Bodemvochtigheidssensor (analoge input GPIO34)
+int WATERNIVEAUPIN = 35;     // Waterniveausensor (analoge input GPIO35)
 
-void setup() { 
+int MOTORPIN1 = 27; 
+int MOTORPIN2 = 26; 
+int MOTORACTIVATIE = 14; 
+
+int vochtigheid = 0; 
+int waterniveau = 0; 
+
+
+void setup() {
+  pinMode(MOTORPIN1, OUTPUT);
+  pinMode(MOTORPIN2, OUTPUT);
+  pinMode(MOTORACTIVATIE, OUTPUT);
  Serial.begin(9600); 
 } 
 
 void loop() { 
-  test_vochtigheid()
+  test_vochtigheid();
+  test_waterniveau();
   delay(1000); 
 } 
 
 void test_vochtigheid() {
-  value= analogRead(SENSE); 
-  value= value/10; 
-  Serial.println(value); 
-  if(value<50) // experimenteren met waarden
+  vochtigheid = analogRead(VOCHTIGHEIDSPIN); 
+  vochtigheid = vochtigheid/10; 
+  Serial.println(vochtigheid); 
+  if(vochtigheid<50) // TODO nog experimenteren met waarden
   { 
-    // DOE IETS
+    motor_open();
+    delay(2000); // Aantal seconden dat het buisje open staat
+    motor_toe();
   }
+}
+
+void test_waterniveau() {
+  waterniveau= analogRead(WATERNIVEAUPIN); 
+  waterniveau= waterniveau/10; 
+  Serial.println(waterniveau); 
+  if(waterniveau<50) // TODO experimenteren met waarden
+  { 
+    stuur_melding()
+  }
+}
+
+void motor_open() {
+  digitalWrite(MOTORPIN1, LOW);
+  digitalWrite(MOTORPIN2, HIGH); 
+}
+
+void motor_toe() {
+  digitalWrite(MOTORPIN1, HIGH);
+  digitalWrite(MOTORPIN2, LOW); 
+}
+
+void stuur_melding() {
+
 }
